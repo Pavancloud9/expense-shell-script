2b03c953ad90f9c07b08bc032672a9009dd2c523
pavan <pavankumar49210@gmail.com>
2026-02-05 09:29:01 +0530

    added

--- mysql.sh
+++ mysql.sh
@@ -7,9 +7,43 @@ then
     exit 1
 fi
 
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo "$1....FAILURE"
+        exit 1
+    else
+        echo "$2....SUCCESS"
+    fi
+}
+
 ###############################################
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log" 
+
+##############
+
+echo "Script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME
+
+dnf list installed mysql-server
+if  [ $? -ne 0 ]
+    then
+
+dnf install mysql-server -y
+    VALIDATE $? "Installing Mysql-server"
+
+else
+    echo "MYSQL-SERVER....ALREADY INSTALLED"
+fi
+
+systemctl enable mysqld
+VALIDATE $? "Enabling mysql-server"
+
+systemctl start mysqld
+VALIDATE $? "Starting mysql-server"
+
+
+
