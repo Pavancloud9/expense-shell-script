bf2acf2c11ae31c4dc394ed12fca4c239d757165
pavan <pavankumar49210@gmail.com>
2026-02-06 11:32:17 +0530

    added

--- backend.sh
+++ backend.sh
@@ -45,7 +45,7 @@ VALIDATE $? "Adding expense user"
 else
     echo "Already Expense ID created...SKIPPING"
 
-mkdir /app
+mkdir -p /app
 VALIDATE $? "Creating App directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
