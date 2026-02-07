8f7ac29634374f102f1340673fd643dff6a18f91
pavan <pavankumar49210@gmail.com>
2026-02-07 12:34:25 +0530

    added

--- mysql.sh
+++ mysql.sh
@@ -28,31 +28,4 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 echo "Script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME
 
-dnf list installed mysql-server
-if  [ $? -ne 0 ]
-    then
-
-dnf install mysql-server -y
-    VALIDATE $? "Installing Mysql-server"
-
-else
-    echo "MYSQL-SERVER....ALREADY INSTALLED"
-fi
-
-systemctl enable mysqld
-VALIDATE $? "Enabling mysql-server"
-
-systemctl start mysqld
-VALIDATE $? "Starting mysql-server"
-
-mysql -h 172.31.20.178 -uroot -pExpenseApp@1 -e 'show databases';
-if [ $? -ne 0 ]
-    then
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "setting root password" 
-else
-    echo "MYSQL ROOT PASSWORD SETUP HAS BEEN DONE...SKIPPING"
-fi
-
-
 
