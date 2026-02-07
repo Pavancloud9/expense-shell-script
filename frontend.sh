6524b7e9b071b95db1f2d1aa990dfbee6c3d5f79
pavan <pavankumar49210@gmail.com>
2026-02-07 19:10:54 +0530

    added

---
+++ frontend.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ROOTID=$(id -u)
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1
+fi
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+LOG_FILE_NAME=$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log
+
+echo "Script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME
